f8d72d98415b7a01094bfeea4867bf424489f6ad
Roasted-Codes <roastedone@icloud.com>
2025-03-31 00:05:44 -0400

    Update start.sh
    ensured SDL AUDIODRIVER and XDG DATA HOME were set correctly. 
    
    Also changed from using an AppImage of Xemu to the latest version just installed to the system.

--- start.sh
+++ start.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # Get the directory where this script is located
-APP_DIR="$(dirname "$(realpath "$0")")"
-# Set the path to the configuration file
-CONFIG_PATH="$APP_DIR/xemu.toml"
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-# Set location for xemu data files
-export XDG_DATA_HOME="$APP_DIR/xemu_data"
-# Set runtime directory, using system default if already defined
-export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
-# Disable audio output
+# Use the xemu.toml file in the same directory as the script
+CONFIG_PATH="$SCRIPT_DIR/xemu.toml"
+
+# Set the data directory (attempting to use XDG specification)
+export XDG_DATA_HOME="$SCRIPT_DIR/xemu_data"
+
+# XDG_RUNTIME_DIR defines a user-specific directory for runtime files
+# It typically points to /run/user/<uid> and is used for:
+# - Temporary runtime files
+# - Communication sockets
+# - State information that doesn't need to persist across reboots
+# This is part of the XDG Base Directory Specification
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
+# Set SDL audio driver to dummy (disables audio output)
 export SDL_AUDIODRIVER=dummy
 
-# Launch xemu with the specified config file in the background
-"$APP_DIR/xemu.AppImage" -config_path "$CONFIG_PATH" &
+# Run xemu with the config_path
+xemu -config_path "$CONFIG_PATH" &
